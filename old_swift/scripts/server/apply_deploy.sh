ca12c2e8a4c1671ddf89eddbe469db3c403b9b4a
NiciusB <nuno@balbona.me>
2023-09-18 23:37:28 +0200

    Delete swift code

--- old_swift/scripts/server/apply_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo cp scripts/server/deploy.service /etc/systemd/system/manifold-trading-bot.service
-sudo systemctl daemon-reload
-sudo systemctl restart manifold-trading-bot
-sudo systemctl enable manifold-trading-bot
--- old_swift/scripts/test_in_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Remove SwiftLintPlugin which causes linux builds to fail
-sed -i.bak '/.plugin(name: "SwiftLintPlugin", package: "SwiftLint")/d' Package.swift
-swift test
