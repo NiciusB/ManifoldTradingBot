2358977f6d90760d6c7d37ef5f378001aa75b00c
NiciusB <niciusb@gmail.com>
2025-10-30 20:05:20 +0100

    Improve env handling

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
+import { load } from "@std/dotenv";
+
+const envRaw = await load({});
 
 export const env = z.object({
   MANIFOLD_API_KEY: z.string().min(1, "MANIFOLD_API_KEY is required"),
   MANIFOLD_API_DEBUG: z.string().optional().default("false"),
-}).parse(Deno.env.toObject());
+}).parse(envRaw);
